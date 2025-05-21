b4edeefd30388edd2d414a7982032dfc13916a08
lucastupai <luc.puentes@duocuc.cl>
2025-05-20 20:28:31 -0400

    Notification

---
+++ src/main/java/com/futbolfanatic/notification_service/model/Notification.java
@@ -0,0 +1,18 @@
+package com.futbolfanatic.notification_service.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+    private String message;
+    private Boolean sent;
+}
