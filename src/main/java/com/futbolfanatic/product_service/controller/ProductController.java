6156644b9a35273d0d5ec068291201a6630a8ebf
lucastupai <luc.puentes@duocuc.cl>
2025-05-20 20:35:26 -0400

    Product Controller

---
+++ src/main/java/com/futbolfanatic/product_service/controller/ProductController.java
@@ -0,0 +1,40 @@
+package com.futbolfanatic.product_service.controller;
+
+import com.futbolfanatic.product_service.model.Product;
+import com.futbolfanatic.product_service.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public List<Product> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @PostMapping
+    public Product create(@RequestBody Product product) {
+        return service.create(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product update(@PathVariable Long id, @RequestBody Product product) {
+        return service.update(id, product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
