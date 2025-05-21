71d613016dfce1bea3624c1aa1447a5cf61fb04e
lucastupai <luc.puentes@duocuc.cl>
2025-05-20 20:30:38 -0400

    Order

---
+++ src/main/java/com/futbolfanatic/order_service/model/Order.java
@@ -0,0 +1,20 @@
+package com.futbolfanatic.order_service.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+    private Long productId;
+    private Integer quantity;
+    private String status; // e.g., "Pending", "Confirmed"
+}
