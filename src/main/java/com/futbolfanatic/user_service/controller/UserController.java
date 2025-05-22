da3985376684eed922852529371950d324bda897
MdeMedina <mamedina770@gmail.com>
2025-05-21 21:00:49 -0400

    Cambios resolucion error 204

--- src/main/java/com/futbolfanatic/user_service/controller/UserController.java
+++ src/main/java/com/futbolfanatic/user_service/controller/UserController.java
@@ -3,6 +3,8 @@ package com.futbolfanatic.user_service.controller;
 import com.futbolfanatic.user_service.model.User;
 import com.futbolfanatic.user_service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,6 +32,7 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         userService.deleteUser(id);
     }
